d5b5ffa7ebccdcddcf08f5a185c5822d56334e52
Peter Hutchison <Peterhutch03@gmail.com>
2022-02-07 10:32:30 -0600

    Initial Commit

---
+++ API Calling Project/API_Calling_ProjectApp.swift
@@ -0,0 +1,17 @@
+//
+//  API_Calling_ProjectApp.swift
+//  API Calling Project
+//
+//  Created by Student on 2/7/22.
+//
+
+import SwiftUI
+
+@main
+struct API_Calling_ProjectApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
